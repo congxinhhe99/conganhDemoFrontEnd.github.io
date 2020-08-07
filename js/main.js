b0cc8e9de655d9fde367b239e1138b279ef1f14f
congxinhhe99 <65597889+congxinhhe99@users.noreply.github.com>
2020-08-07 19:45:00 +0700

    add some css

--- js/main.js
+++ js/main.js
@@ -15,16 +15,16 @@ var productImg = document.getElementById("productImg");
 
 var SmallImg = document.getElementsByClassName("small-img");
 
-SmallImg[0].onclick = function() {
+SmallImg[0].onmouseover = function() {
     productImg.src = SmallImg[0].src;
 }
-SmallImg[1].onclick = function() {
+SmallImg[1].onmouseover = function() {
     productImg.src = SmallImg[1].src;
 }
-SmallImg[2].onclick = function() {
+SmallImg[2].onmouseover = function() {
     productImg.src = SmallImg[2].src;
 }
-SmallImg[3].onclick = function() {
+SmallImg[3].onmouseover = function() {
     productImg.src = SmallImg[3].src;
 }
 
