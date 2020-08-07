e454f44fb07f90bde6b1dd3aefa5a61ec1a6ffab
congxinhhe99 <65597889+congxinhhe99@users.noreply.github.com>
2020-08-07 22:29:26 +0700

    addition Jquery

--- js/main.js
+++ js/main.js
@@ -15,16 +15,16 @@ var productImg = document.getElementById("productImg");
 
 var SmallImg = document.getElementsByClassName("small-img");
 
-SmallImg[0].onmouseover = function() {
+SmallImg[0].onmouseover = function () {
     productImg.src = SmallImg[0].src;
 }
-SmallImg[1].onmouseover = function() {
+SmallImg[1].onmouseover = function () {
     productImg.src = SmallImg[1].src;
 }
-SmallImg[2].onmouseover = function() {
+SmallImg[2].onmouseover = function () {
     productImg.src = SmallImg[2].src;
 }
-SmallImg[3].onmouseover = function() {
+SmallImg[3].onmouseover = function () {
     productImg.src = SmallImg[3].src;
 }
 
